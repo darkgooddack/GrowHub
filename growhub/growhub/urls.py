24c5346d21efa3f99b74286aaa3f5d801b3dc5dd
ulyana <ulyanaworkpage@gmail.com>
2025-08-21 16:10:04 +0300

    protection

--- growhub/growhub/urls.py
+++ growhub/growhub/urls.py
@@ -1,8 +1,32 @@
+import base64
+from functools import wraps
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+from growhub.settings import SWAGGER_PASSWORD, SWAGGER_USER
+
+
+def swagger_password_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if auth_header:
+            auth_method, auth = auth_header.split(' ', 1)
+            if auth_method.lower() == 'basic':
+                decoded_auth = base64.b64decode(auth).decode('utf-8')
+                username, password = decoded_auth.split(':', 1)
+                if username == SWAGGER_USER and password == SWAGGER_PASSWORD:
+                    return view_func(request, *args, **kwargs)
+        response = HttpResponse('Unauthorized', status=401)
+        response['WWW-Authenticate'] = 'Basic realm="Swagger"'
+        return response
+    return _wrapped_view
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,6 +42,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('projects.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
+    path('swagger/', swagger_password_required(
+        schema_view.with_ui('swagger', cache_timeout=0)
+    ), name='schema-swagger-ui'),
 ]
+
--- growhub/users/apps.py
+++ growhub/users/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from growhub.settings import SUPERUSER_NAME, SUPERUSER_PASSWORD
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        User = get_user_model()
+        username = SUPERUSER_NAME
+        password = SUPERUSER_PASSWORD
+        email = f'{username}@example.com'
+
+        if not User.objects.filter(username=username).exists():
+            print(f'Создаём superuser {username}')
+            User.objects.create_superuser(username=username, email=email, password=password)
